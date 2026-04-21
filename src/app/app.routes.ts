1970f0207faf9ca278481c9f16ce92a385cb6c3b
Mohamad Alabudi <mohamadalabudi42@gmail.com>
2026-04-20 22:01:19 -0400

    feat: added basic login page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,11 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
+    path: 'login',
+    loadComponent: () =>
+      import('./pages/login/login.component').then(m => m.LoginComponent)
+  },
+  {
     path: 'register',
     loadComponent: () =>
       import('./pages/register/register.component').then(m => m.RegisterComponent)
