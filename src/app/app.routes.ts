36f9110127890a0a45bd663573dd2f23917bdd28
Mohamad Alabudi <mohamadalabudi42@gmail.com>
2026-04-21 17:40:01 -0400

    feat: added blank menu page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './core/guards/auth.guard';
 
 export const routes: Routes = [
   {
@@ -11,5 +12,12 @@ export const routes: Routes = [
     loadComponent: () =>
       import('./pages/register/register.component').then(m => m.RegisterComponent)
   },
+  {
+    path: 'menu',
+    loadComponent: () =>
+      import('./pages/menu/menu.component').then(m => m.MenuComponent),
+    canActivate: [authGuard]
+  },
+  {path: '', redirectTo: 'menu', pathMatch: 'full'},
   { path: '**', redirectTo: 'login' }
 ];
