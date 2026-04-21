763c53f5665cab463707ea1fa5d824c068c6d769
Mohamad Alabudi <mohamadalabudi42@gmail.com>
2026-04-20 22:01:19 -0400

    feat: added basic login page

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,12 +1,52 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { AuthenticationRequest } from '../../models/auth.models';
+import { AuthService } from '../../core/services/auth.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, RouterLink],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  form: AuthenticationRequest = {
+    email: '',
+    password: ''
+  };
 
+  errorMessage = '';
+  loading = false;
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  onSubmit() {
+    this.errorMessage = '';
+    this.loading = true;
+
+    this.authService.login(this.form).subscribe({
+      next: () => {
+        if (this.authService.isAdmin()) {
+          this.router.navigate(['/admin/orders']);
+        } else {
+          this.router.navigate(['/menu']);
+        }
+      },
+      error: (err) => {
+        this.loading = false;
+        this.errorMessage = err.status == 403 
+        ? 'Invalid email or password' 
+        : 'Something went wrong. Please try again.';
+      },
+      complete: () => {
+        this.loading = false;
+      }
+    });
+  }
 }
