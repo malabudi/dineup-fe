f960cbe1361ee68f98c9ea1077804ce995347da3
Mohamad Alabudi <mohamadalabudi42@gmail.com>
2026-04-22 17:47:38 -0400

    feat: added form validation to register page

---
+++ src/app/core/constants/validation.constants.ts
@@ -0,0 +1,3 @@
+export const EMAIL_REGEX = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+export const PASSWORD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/; // Minimum 8 characters, at least one letter and one number
+export const NAME_REGEX = /^[A-Za-z]+$/; // Only letters, no spaces or special characters
