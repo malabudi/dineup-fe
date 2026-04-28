9cfbc784cb6202baefb969066e9580d150521d97
Mohamad Alabudi <mohamadalabudi42@gmail.com>
2026-04-27 21:08:48 -0400

    feat: added navbar component

---
+++ src/app/shared/navbar/navbar.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router, RouterLink } from '@angular/router';
+import { CartService } from '../../services/cart.service';
+import { AuthService } from '../../core/services/auth.service';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [CommonModule, RouterLink],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss'
+})
+export class NavbarComponent implements OnInit {
+  
+  itemCount$!: Observable<number>;
+
+  constructor(
+    private cartService: CartService,
+    private authService: AuthService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.itemCount$ = this.cartService.itemCount$;
+  }
+
+  logout(): void {
+    this.authService.logout();
+  }
+}
